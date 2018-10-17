851cb538fe11c2d3ecfd87df0a2b497073861a9d
Michael <mbutcherus@gmail.com>
2018-10-17 15:38:18 -0700

    App collects inputs, spits back out regular price.  No logic connnected up for figuring discounts

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,32 @@ function Ticket(movie, time, age) {
   this.movie = movie;
   this.time = time;
   this.age = age;
+  var price = 10;
+    // if (movieDiscount === true) {
+    //   price = price - 1;
+    // };
+    // if (timeDiscount === true) {
+    //   price = price - 1;
+    // };
+    // if (ageDiscount === true) {
+    //   price = price - 1;
+    // };
   this.price = price;
+};
 
+// Ticket.prototype.finalPrice = function() {
+//   var price = 10;
+//   if (movieDiscount === true) {
+//     price -= 1;
+//   };
+//   if (timeDiscount === true) {
+//     price -= 1;
+//   };
+//   if (ageDiscount === true) {
+//     price -= 1;
+//   };
+//   return this.price
+// };
 
 var movieDiscount = function(movie) {
   if (movie === "old") {
@@ -13,7 +37,7 @@ var movieDiscount = function(movie) {
   };
 };
 function timeDiscount(time) {
-  if (time ==== "matinee") {
+  if (time === "matinee") {
     return true;
   } else {
     return false;
@@ -28,11 +52,13 @@ function ageDiscount(age) {
 };
 
 $(document).ready(function() {
-  $(form#"tickets-form").submit(function(event) {
-  event.preventDefault();
-  var movie = $("#movies").val();
-  var time = $("#times").val();
-  var age = $("input#age").val();
-  var newTicket = new Ticket(movie, time, age);
+  $("form#tickets-form").submit(function(event) {
+    event.preventDefault();
+    var movie = $("#movies").val();
+    var time = $("#times").val();
+    var age = $("input#age").val();
+    var newTicket = new Ticket(movie, time, age);
+    $(".price").text(newTicket.price);
+    $("#price-box").show();
   });
 });
